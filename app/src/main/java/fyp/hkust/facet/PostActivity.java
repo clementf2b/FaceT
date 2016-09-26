27a479cd16741bcc06e77cc5128212ca6d1da64a
clementf2b <ng2b30@hotmail.com>
2016-09-26 21:28:12 +0800

    copy
    initial commit
    
    commit

--- app/src/main/java/fyp/hkust/facet/PostActivity.java
+++ app/src/main/java/fyp/hkust/facet/PostActivity.java
@@ -1,13 +1,67 @@
 package fyp.hkust.facet;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+
+
+    private ImageButton mSelectBtn;
+    private EditText mProductTitle;
+    private EditText mProductDesc;
+    private Button mSubmitBtn;
+
+    private static final int GALLERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mSelectBtn = (ImageButton) findViewById(R.id.imageButton);
+        mProductTitle = (EditText) findViewById(R.id.photo_title);
+        mProductDesc = (EditText) findViewById(R.id.photo_desc);
+        mSubmitBtn = (Button) findViewById(R.id.submit_btn);
+
+        mSelectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,GALLERY_REQUEST);
+            }
+        });
+
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPosting();
+            }
+        });
+    }
+
+    private void startPosting() {
+        String titl_val =
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK)
+        {
+            Uri imageUri = data.getData();
+            mSelectBtn.setImageURI(imageUri);
+        }
     }
 }
