dd35a59a46feaf42ee0f6a7912fdce1085968be5
ttchiuaa <daviddach917@gmail.com>
2016-10-16 01:06:36 +0800

    set transparent status bar

--- app/src/main/java/fyp/hkust/facet/MainMenuActivity.java
+++ app/src/main/java/fyp/hkust/facet/MainMenuActivity.java
@@ -1,9 +1,12 @@
 package fyp.hkust.facet;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -16,6 +19,14 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            // Translucent status bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
 
         photoCameraBtn = (ImageButton)findViewById(R.id.photo_camera);
         shoppingBtn = (ImageButton)findViewById(R.id.shopping_button);
