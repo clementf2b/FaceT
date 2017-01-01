ad384fb61b2cada330d1ce757612decba5f75cc7
Clement Ng <ng2b30@hotmail.com>
2017-01-01 17:49:23 +0800

    added nav bar demo and category layout

--- app/src/main/java/fyp/hkust/facet/AccountActivity.java
+++ app/src/main/java/fyp/hkust/facet/AccountActivity.java
@@ -2,18 +2,30 @@ package fyp.hkust.facet;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,17 +34,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-
-public class AccountActivity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity{
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String NAV_ITEM_ID = "nav_index";
     private ImageButton profilePic;
     private TextView mNameField;
     private Uri mImageUri;
@@ -45,10 +55,61 @@ public class AccountActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private LinearLayoutManager horizontalLayoutManagaer;
 
+    DrawerLayout drawerLayout;
+    TextView contentView;
+    private int navItemId;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        //start
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setBackground(new ColorDrawable(Color.parseColor("#00000000")) );
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Toast.makeText(AccountActivity.this, menuItem.getTitle() + " pressed", Toast.LENGTH_LONG).show();
+                navigateTo(menuItem);
+
+                drawerLayout.closeDrawers();
+
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        if(null != savedInstanceState){
+            navItemId = savedInstanceState.getInt(NAV_ITEM_ID, R.id.nav_camera);
+        }
+        else{
+            navItemId = R.id.nav_camera;
+        }
+
+        navigateTo(view.getMenu().findItem(navItemId));
+
+        //end
 
         mAuth = FirebaseAuth.getInstance();
         mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
@@ -124,6 +185,78 @@ public class AccountActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.account_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void navigateTo(MenuItem menuItem){
+       // contentView.setText(menuItem.getTitle());
+
+        navItemId = menuItem.getItemId();
+        //Check to see which item was being clicked and perform appropriate action
+        switch (navItemId) {
+            //Replacing the main content with ContentFragment Which is our Inbox View;
+            case R.id.nav_camera:
+                navItemId = 0;
+                break;
+            case R.id.nav_gallery:
+                navItemId = 1;
+                break;
+            case R.id.nav_manage:
+                navItemId = 2;
+                break;
+            case R.id.nav_send:
+                navItemId = 3;
+                break;
+            case R.id.nav_share:
+                navItemId = 4;
+                break;
+            case R.id.nav_slideshow:
+                startActivity(new Intent(AccountActivity.this, CategoryActivity.class));
+                break;
+            case R.id.navigation_view:
+                break;
+            default:
+                navItemId = 0;
+        }
+        Snackbar.make(drawerLayout, navItemId + " Clicked" ,Snackbar.LENGTH_SHORT).show();
+        menuItem.setChecked(true);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(NAV_ITEM_ID, navItemId);
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
 
@@ -147,7 +280,8 @@ public class AccountActivity extends AppCompatActivity {
         };
 
         recentlyMatchProductList.setAdapter(firebaseRecyclerAdapter);
-
+        recentlyMatchProductList.setNestedScrollingEnabled(false);
+        recentlyMatchProductList.isScrollbarFadingEnabled();
     }
 
     public static class RecenlyMatchProductViewHolder extends RecyclerView.ViewHolder{
---
+++ app/src/main/java/fyp/hkust/facet/CategoryActivity.java
@@ -0,0 +1,89 @@
+package fyp.hkust.facet;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    private static final String TAG = "CategoryActivity";
+    private static final String NAV_ITEM_ID = "nav_index";
+    DrawerLayout drawerLayout;
+    TextView contentView;
+    private int navItemId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        //start
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setBackground(new ColorDrawable(Color.parseColor("#00000000")) );
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Toast.makeText(CategoryActivity.this, menuItem.getTitle() + " pressed", Toast.LENGTH_LONG).show();
+                navigateTo(menuItem);
+
+                drawerLayout.closeDrawers();
+
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        if(null != savedInstanceState){
+            navItemId = savedInstanceState.getInt(NAV_ITEM_ID, R.id.nav_camera);
+        }
+        else{
+            navItemId = R.id.nav_camera;
+        }
+
+        navigateTo(view.getMenu().findItem(navItemId));
+
+        //end
+    }
+
+    private void navigateTo(MenuItem menuItem){
+        // contentView.setText(menuItem.getTitle());
+
+        navItemId = menuItem.getItemId();
+        menuItem.setChecked(true);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(NAV_ITEM_ID, navItemId);
+    }
+
+}
