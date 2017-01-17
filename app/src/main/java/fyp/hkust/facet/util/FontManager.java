07cee0c9ba8b76fc2c489656398fbff87a9600a2
Clement Ng <ng2b30@hotmail.com>
2017-01-17 19:12:49 +0800

    add new font manager

---
+++ app/src/main/java/fyp/hkust/facet/util/FontManager.java
@@ -0,0 +1,35 @@
+package fyp.hkust.facet.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by ClementNg on 17/1/2017.
+ */
+
+public class FontManager {
+
+    public static final String ROOT = "fonts/",
+            APP_FONT = ROOT + "HiraginoSansGBW3.ttf";
+
+    public static Typeface getTypeface(Context context, String font) {
+        return Typeface.createFromAsset(context.getAssets(), font);
+    }
+
+    public static void markAsIconContainer(View v, Typeface typeface) {
+        if (v instanceof ViewGroup) {
+            ViewGroup vg = (ViewGroup) v;
+            for (int i = 0; i < vg.getChildCount(); i++) {
+                View child = vg.getChildAt(i);
+                markAsIconContainer(child,typeface);
+            }
+        } else if (v instanceof TextView) {
+            ((TextView) v).setTypeface(typeface);
+        }
+    }
+
+
+}
