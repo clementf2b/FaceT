fb77c297ad6319895fb08f29729f44fbf4c7cc7c
Clement Ng <ng2b30@hotmail.com>
2017-01-18 20:59:49 +0800

    add the Dlib for landmark detection and the function

--- app/src/main/java/fyp/hkust/facet/activity/SwapFaceActivity.java
+++ app/src/main/java/fyp/hkust/facet/activity/SwapFaceActivity.java
@@ -1,19 +1,79 @@
 package fyp.hkust.facet.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.UiThread;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.dexafree.materialList.card.Card;
+import com.dexafree.materialList.card.provider.BigImageCardProvider;
+import com.tzutalin.dlib.Constants;
+import com.tzutalin.dlib.FaceDet;
+import com.tzutalin.dlib.VisionDetRet;
 
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import fyp.hkust.facet.R;
 import fyp.hkust.facet.util.FontManager;
+import fyp.hkust.facet.util.FileUtils;
+import fyp.hkust.facet.util.ImageUtils;
+import hugo.weaving.DebugLog;
 
 import static org.opencv.imgproc.Imgproc.INTER_LINEAR;
 
 public class SwapFaceActivity extends AppCompatActivity {
+
+    private static final int RESULT_LOAD_IMG = 1;
+    private static final int REQUEST_CODE_PERMISSION = 2;
+
+    private static final String TAG = "MainActivity";
+
+    // Storage Permissions
+    private static String[] PERMISSIONS_REQ = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
+    protected String mTestImgPath;
+    // UI
+
+    protected ImageView mDetectResultImage;
+    protected FloatingActionButton mFabActionBt;
+
+    FaceDet mFaceDet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +82,28 @@ public class SwapFaceActivity extends AppCompatActivity {
 
         Typeface fontType = FontManager.getTypeface(getApplicationContext(), FontManager.APP_FONT);
         FontManager.markAsIconContainer(findViewById(R.id.activity_swap_face_layout), fontType);
+
+        mFabActionBt = (FloatingActionButton) findViewById(R.id.fab);
+        mDetectResultImage = (ImageView) findViewById(R.id.detect_result_image);
+
+        mFabActionBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SwapFaceActivity.this, "Pick one image", Toast.LENGTH_SHORT).show();
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+            }
+        });
+        // Just use hugo to print log
+        isExternalStorageWritable();
+        isExternalStorageReadable();
+
+        // For API 23+ you need to request the read/write permissions even if they are already in your manifest.
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+
+        if (currentapiVersion >= Build.VERSION_CODES.M) {
+            verifyPermissions(this);
+        }
     }
 
 
@@ -71,5 +153,209 @@ public class SwapFaceActivity extends AppCompatActivity {
 //        }
 //
 //    }
+
+    /**
+     * Checks if the app has permission to write to device storage or open camera
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    @DebugLog
+    private static boolean verifyPermissions(Activity activity) {
+        // Check if we have write permission
+        int write_permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int read_persmission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int camera_permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.CAMERA);
+
+        if (write_permission != PackageManager.PERMISSION_GRANTED ||
+                read_persmission != PackageManager.PERMISSION_GRANTED ||
+                camera_permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_REQ,
+                    REQUEST_CODE_PERMISSION
+            );
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    @DebugLog
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    @DebugLog
+    private boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    @DebugLog
+    protected void demoStaticImage() {
+        if (mTestImgPath != null) {
+            Log.d(TAG, "demoStaticImage() launch a task to det");
+            runDetectAsync(mTestImgPath);
+        } else {
+            Log.d(TAG, "demoStaticImage() mTestImgPath is null, go to gallery");
+            Toast.makeText(SwapFaceActivity.this, "Pick an image to run algorithms", Toast.LENGTH_SHORT).show();
+            // Create intent to Open Image applications like Gallery, Google Photos
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            Toast.makeText(SwapFaceActivity.this, "Demo using static images", Toast.LENGTH_SHORT).show();
+            demoStaticImage();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK && null != data) {
+                // Get the Image from data
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                mTestImgPath = cursor.getString(columnIndex);
+                cursor.close();
+                if (mTestImgPath != null) {
+                    runDetectAsync(mTestImgPath);
+                    Toast.makeText(this, "Img Path:" + mTestImgPath, Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    // ==========================================================
+    // Tasks inner class
+    // ==========================================================
+    private ProgressDialog mDialog;
+
+    @NonNull
+    protected void runDetectAsync(@NonNull String imgPath) {
+        showDiaglog();
+
+        final String targetPath = Constants.getFaceShapeModelPath();
+        if (!new File(targetPath).exists()) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(SwapFaceActivity.this, "Copy landmark model to " + targetPath, Toast.LENGTH_SHORT).show();
+                }
+            });
+            FileUtils.copyFileFromRawToOthers(getApplicationContext(), R.raw.shape_predictor_68_face_landmarks, targetPath);
+        }
+        // Init
+        if (mFaceDet == null) {
+            mFaceDet = new FaceDet(Constants.getFaceShapeModelPath());
+        }
+
+        Log.d(TAG, "Image path: " + imgPath);
+        List<VisionDetRet> faceList = mFaceDet.detect(imgPath);
+
+        mDetectResultImage.setImageBitmap(drawRect(imgPath, faceList, Color.GREEN));
+        dismissDialog();
+    }
+
+    @UiThread
+    protected void showDiaglog() {
+        mDialog = ProgressDialog.show(SwapFaceActivity.this, "Wait", "Person and face detection", true);
+    }
+
+    @UiThread
+    protected void dismissDialog() {
+        if (mDialog != null) {
+            mDialog.dismiss();
+        }
+    }
+
+    @DebugLog
+    protected Bitmap drawRect(String path, List<VisionDetRet> results, int color) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 1;
+        Bitmap bm = BitmapFactory.decodeFile(path, options);
+        android.graphics.Bitmap.Config bitmapConfig = bm.getConfig();
+        // set default bitmap config if none
+        if (bitmapConfig == null) {
+            bitmapConfig = android.graphics.Bitmap.Config.ARGB_8888;
+        }
+        // resource bitmaps are imutable,
+        // so we need to convert it to mutable one
+        bm = bm.copy(bitmapConfig, true);
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        // By ratio scale
+        float aspectRatio = bm.getWidth() / (float) bm.getHeight();
+
+        final int MAX_SIZE = 512;
+        int newWidth = MAX_SIZE;
+        int newHeight = MAX_SIZE;
+        float resizeRatio = 1;
+        newHeight = Math.round(newWidth / aspectRatio);
+        if (bm.getWidth() > MAX_SIZE && bm.getHeight() > MAX_SIZE) {
+            Log.d(TAG, "Resize Bitmap");
+            bm = getResizedBitmap(bm, newWidth, newHeight);
+            resizeRatio = (float) bm.getWidth() / (float) width;
+            Log.d(TAG, "resizeRatio " + resizeRatio);
+        }
+
+        // Create canvas to draw
+        Canvas canvas = new Canvas(bm);
+        Paint paint = new Paint();
+        paint.setColor(color);
+        paint.setStrokeWidth(2);
+        paint.setStyle(Paint.Style.STROKE);
+        // Loop result list
+        for (VisionDetRet ret : results) {
+            Rect bounds = new Rect();
+            bounds.left = (int) (ret.getLeft() * resizeRatio);
+            bounds.top = (int) (ret.getTop() * resizeRatio);
+            bounds.right = (int) (ret.getRight() * resizeRatio);
+            bounds.bottom = (int) (ret.getBottom() * resizeRatio);
+            canvas.drawRect(bounds, paint);
+            // Get landmark
+            ArrayList<Point> landmarks = ret.getFaceLandmarks();
+            for (Point point : landmarks) {
+                int pointX = (int) (point.x * resizeRatio);
+                int pointY = (int) (point.y * resizeRatio);
+                canvas.drawCircle(pointX, pointY, 2, paint);
+            }
+        }
+
+        return bm;
+    }
+
+    @DebugLog
+    protected Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bm, newWidth, newHeight, true);
+        return resizedBitmap;
+    }
 
 }
